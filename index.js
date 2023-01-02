ba5049d1e8fceaf2c572a59131aea19f1891df16
Ivan Escobar <ivnescbr1989@gmail.com>
2023-01-02 13:23:49 -0500

    added questions & file format

--- index.js
+++ index.js
@@ -1,10 +1,59 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: `input`,
+        message: `What is the title of the project`,
+        name: `title`
+    },
+    {
+        type: `input`,
+        message: `Provide a short description of the project`,
+        name: `description`
+    },
+    {
+        type: `input`,
+        message: `Provide installation instructions if any`,
+        name: `install`
+    },
+    {
+        type: `input`,
+        message: `Provide information for the usage of project`,
+        name: `usage`
+    },
+    {
+        type: `input`,
+        message: `Provide contribution guidelines`,
+        name: `contribution`
+    },
+    {
+        type: `input`,
+        message: `Provide test instructions if any`,
+        name: `test`
+    },
+    { 
+        type: `list`,
+        message: `Choose a license for your project.`,
+        choices: [`GNU AGPLv3`, `Mozilla Public License 2.0`, `Apache License 2.0`, `MIT License`, `N/A`],
+        name: `license`
+    },
+    {
+        type: `input`,
+        message: `Provide GitHub user name`,
+        name: `github`
+    },
+    {
+        type: `input`,
+        message: `Provide and email`,
+        name: `email`
+    }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+//function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
 function init() {}
